c29c45d322446fca6e6f5335c205f8f5c793ff89
Helen L <htslin103@gmail.com>
2022-08-14 22:44:06 -0600

    Added ErrorModal component and unique User ID

--- 01-starting-project/src/components/Users/AddUser.js
+++ 01-starting-project/src/components/Users/AddUser.js
@@ -3,13 +3,15 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 //Here we want the user to be able to add name and age and enter it
 import classes from './AddUser.module.css';
+import ErrorModal from '../UI/ErrorModal';
 import Button from '../UI/Button';
-import UsersList from './UsersList';
+
 
 const AddUser = props => {
     //useState constants
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
     
     const addUserHandler = (event) =>
     {      
@@ -19,11 +21,19 @@ const AddUser = props => {
         //error handling
         //If empty string or Age is 0, end fxn
         if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name and age (non-empty values) .'
+            });
             return;
         }
         //The +before enteredAge forces it to be an int
         if(+enteredAge < 1)
         {
+            setError({
+                title: 'Invalid age',
+                message: 'Please enter a valid age (>0)'
+            });
             return;
         }
         
@@ -42,10 +52,16 @@ const AddUser = props => {
         setEnteredAge(event.target.value);       
     };
 
-    return(  
-        //since Card is our own custom component, it doesn't know how to use className property so we have to write that
-        //within our class component we called it className. If we picked something like ccsClassname then we would need to reference that in the card element 
-    <Card className={classes.input}>
+    const errorHandler = () => {
+        setError(null);
+    }
+
+    return(
+    <div>
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+        {/* //since Card is our own custom component, it doesn't know how to use className property so we have to write that
+        //within our class component we called it className. If we picked something like ccsClassname then we would need to reference that in the card element */}
+     <Card className={classes.input}> 
         <form onSubmit={addUserHandler}>
             <label htmlFor='username'>Username</label>
             <input id="username" type="text" value = {enteredUsername} onChange={usernameChangeHandler}/>
@@ -54,7 +70,8 @@ const AddUser = props => {
             {/* Our own custom button */}
             <Button type="submit">Submit</Button>
         </form>
-    </Card>
+     </Card>
+    </div>
     );
 };
 
